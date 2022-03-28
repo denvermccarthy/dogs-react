0354f733f03862b35879e163dff1ef247641c7f0
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-28 12:29:15 -0700

    set up first route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './view/Home/Home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path={'/'}>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
