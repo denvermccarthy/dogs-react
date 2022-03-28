0af68589a209ff0e2ece87052af20e4937a2b202
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-28 12:58:38 -0700

    fetch dog by id works and useparams fetches correct dog

--- src/components/DogDetail/DogDetail.js
+++ src/components/DogDetail/DogDetail.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchDogById } from '../../services/dogs';
 
 export default function DogDetail() {
-  return <div>DogDetail</div>;
+  const [dog, setDog] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await fetchDogById(id);
+      setDog(data);
+    };
+    fetch();
+  }, [id]);
+
+  if (!dog) return <div>loading...</div>;
+
+  return <div>{dog.name}</div>;
 }
--- src/services/dogs.js
+++ src/services/dogs.js
@@ -4,3 +4,8 @@ export const fetchDogs = async () => {
   const resp = await client.from('dogs').select();
   return checkError(resp);
 };
+
+export const fetchDogById = async (id) => {
+  const resp = await client.from('dogs').select().match({ id }).single();
+  return checkError(resp);
+};
